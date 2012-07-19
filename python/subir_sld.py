de5e0b7a1cceaef7ad68fe2f38a36ea14212265b
Sylvain Lesage <severo@rednegra.net>
2012-07-19 18:42:01 -0400

    mejora de la calidad del codigo: uso de las excepciones, de funciones, de curses para la interactividad + interactividad para manejar los casos sin solucion obvia

--- python/subir_sld.py
+++ python/subir_sld.py
@@ -7,51 +7,149 @@
 from geoserver.catalog import Catalog
 import sys
 import os
+import curses
+import time
 
-# Datos para la conexion a GeoServer
-user = 'admin_geobolivia'
-password = 'YGoL3lSJYCwjCLDC'
-url_geoserver = "http://www-dev.geo.gob.bo/geoserver/rest"
+# Excepciones manejadas por gsconfig
 
-# Verificamos que se dio un argumento en la linea de comando
-if len(sys.argv) < 2:
-	print "Dar el archivo SLD como argumento: %s /path/to/archivo.sld" % sys.argv[0]
-	quit()
+# UploadError
+# ConflictingDataError
+# AmbiguousRequestError
+# FailedRequestError
+# NotImplementedError
 
-# Verificamos que el archivo dado en la linea de comando existe
-# (sacamos los eventuales ", ', espacio)
-#archivo = sys.argv[1].strip(' \"\'')
-archivo = sys.argv[1]
-if not os.path.isfile(archivo):
-	print "El archivo %s no existe" % archivo
-	quit()
+# Excepcion
+# ExpatError
+# SyntazError
+
+# Funcion de conexion a GeoServer
+def connectar_geoserver(stdscr):
+	# Datos para la conexion a GeoServer
+	user = 'admin_geobolivia'
+	password = 'YGoL3lSJYCwjCLDC'
+	url_geoserver = "http://www-dev.geo.gob.bo/geoserver/rest"
+
+	# Conexion a GeoServer
+	cat = Catalog(url_geoserver, user, password)
+
+	# Probar la conexion con una consulta simple
+	try:
+		all_workspaces = cat.get_workspaces()
+	except Exception as e:
+		# Salio una excepcion: la mostramos y relanzamos la excepcion
+		stdscr.addstr("Error durante la conexion al GeoServer:\n%s\n" % e)
+		raise
+
+	return cat
+
+# Funcion de recuperacion de los argumentos
+def recuperar_archivo(stdscr):
+	# Verificamos que se dio un argumento en la linea de comando
+	if len(sys.argv) < 2:
+		stdscr.addstr("Dar el archivo SLD como argumento: %s /path/to/archivo.sld\n" % sys.argv[0])
+		return None,None
+
+	# Verificamos que el archivo dado en la linea de comando existe
+	# (sacamos los eventuales ", ', espacio)
+	#archivo = sys.argv[1].strip(' \"\'')
+	archivo = sys.argv[1]
+	if not os.path.isfile(archivo):
+		stdscr.addstr("El archivo %s no existe\n" % archivo)
+		return None,None
 	
-# Verificamos que la extension es SLD (.sld)
-fileName, fileExtension = os.path.splitext(archivo)
-baseArchivo = os.path.basename(fileName)
-if fileExtension != ".sld":
-	print "El archivo %s no tiene la extension SLD" % archivo
-	quit()
+	# Verificamos que la extension es SLD (.sld)
+	fileName, fileExtension = os.path.splitext(archivo)
+	if fileExtension != ".sld":
+		stdscr.addstr("El archivo %s no tiene la extension SLD\n" % archivo)
+		return None,None
 
-# Salida grafica
-print "Subimos el archivo %s de estilo en el servidor http://www-dev.geo.gob.bo/geoserver/\n" % archivo
+	# Base del archivo (nombre del estilo)
+	nombreEstilo = os.path.basename(fileName)
 
-# Conexion a GeoServer
-cat = Catalog(url_geoserver, user, password)
+	# Todo esta OK - retornamos el archivo
+	return archivo,nombreEstilo
 
-# Verificamos que funciono la conexion
-if not cat:
-	print "La conexion con el GeoServer fallo"
-	quit()
+# Funcion llamada justo antes de salir
+def salir(stdscr,mensaje=""):
+	if mensaje:
+		stdscr.addstr(mensaje)
 
-# Miramos si ya existe el estilo
-estilo = cat.get_style(baseArchivo)
-if estilo:
-	print "Ya existe el estilo %s en el servidor" % estilo.name
-	quit()
+	stdscr.addstr("Cualquier tecla para salir.\n")
+	c = stdscr.getch()
 
-# No existe el estilo, lo subimos
-# para el uso de "with", ver http://effbot.org/zone/python-with-statement.htm
-# -> el archivo se cerrara automaticamente
-with open(archivo) as f:
-	cat.create_style(baseArchivo, f.read())
+	return None
+
+def main(stdscr):
+	# Recuperar el nombre del archivo
+	archivo,nombreEstilo = recuperar_archivo(stdscr)
+	if not archivo:
+		return salir(stdscr,"La recuperacion del archivo SLD fallo\n")
+
+	# Verificamos que funciono la conexion
+	try:
+		cat =  connectar_geoserver(stdscr)
+	except Exception as e:
+		return salir(stdscr,"La conexion con el GeoServer fallo\n")
+
+	# Salida grafica
+	stdscr.addstr("Subimos el archivo %s de estilo en el servidor http://www-dev.geo.gob.bo/geoserver/\n" % archivo)
+
+	# Miramos si ya existe el estilo - preguntar que hacer
+	try:
+		estilo = cat.get_style(nombreEstilo)
+	except Exception as e:
+		return salir(stdscr,"No se pudo recuperar el estilo %s\n", nombreEstilo)
+		
+	if estilo:
+		i = 3
+		while i > 0:
+			i -=1
+			stdscr.addstr("\nEl estilo '%s' ya existe en el servidor. [R]emplazar, [D]ejar la version anterior o [B]orrarla sin subir la nueva ?\n" % estilo.name)
+			c = stdscr.getch()
+			if c == ord('R'):
+				# Existe el estilo, subimos una nueva version (remplazamos)
+				with open(archivo) as f:
+					try:
+						cat.create_style(nombreEstilo, f.read(), overwrite=True)
+						stdscr.addstr("Estilo remplazado en el servidor\n")
+					except Exception as e:
+						return salir(stdscr,"Salio un error al remplazar el archivo:\n%s\n" % e)
+				break # Exit the while()
+			elif c == ord('B'):
+				# Existe el estilo, lo borramos
+				try:
+					cat.delete(estilo,purge=True)
+					stdscr.addstr("Estilo borrado en el servidor\n")
+				except Exception as e:
+					return salir(stdscr,"Salio un error al borrar el estilo en el servidor:\n%s\n" % e)
+				break # Exit the while()
+			elif c == ord('D'):
+				# No hacemos nada
+				stdscr.addstr("Dejamos la version anterior del estilo en el servidor\n")
+				break # Exit the while()
+		if i == 0:
+			stdscr.addstr("No se entiende - dejamos la version anterior del estilo en el servidor\n")
+	else:
+		# No existe el estilo, lo subimos
+		# para el uso de "with", ver http://effbot.org/zone/python-with-statement.htm
+		# -> el archivo se cerrara automaticamente
+		with open(archivo) as f:
+			try:
+				cat.create_style(nombreEstilo, f.read())
+				stdscr.addstr("Estilo subido en el servidor\n")
+			except Exception as e:
+				return salir(stdscr,"Salio un error al subir el archivo:\n%s\n" % e)
+# http://jira.codehaus.org/browse/GEOS-3621
+
+	# Todo salio bien - mensaje para terminar la funcion
+	return salir(stdscr)
+
+# Creacion de la pantalla con curses
+# ver http://docs.python.org/howto/curses.html#curses-howto para un tutorial sobre curses
+curses.wrapper(main)
+
+# Esperamos un segundo antes de terminar
+time.sleep(0.2)
+
+# Fin
+quit()
