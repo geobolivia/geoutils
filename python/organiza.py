d8d8e3a4cfd903ef2975930edcc614c193eb95fd
jhuanca <jhuanca@geo.gob.bo>
2012-10-17 11:08:30 -0400

    Cambios en el script de Diana para que funcione con la maquina de Wilson

--- python/organiza.py
+++ python/organiza.py
@@ -11,32 +11,32 @@ import fnmatch
 import shutil
 
 # read file csv
-os.chdir('/home/diana/geoutils/python/DATA')
+os.chdir('/home/wilson/pythonDiana/DATA')
 arch = 'TLayers.csv'
-for i,layer in enumerate(all_layers):
-	b = layer.name
-	a = layer.resource.metadata_links
-	that_layer = cat.get_layer(b)
-	that_style = that_layer.default_style
-	print that_style.name
+datos = open(arch, "rb")
+
 # download metadata and pdf
 for row in datos:
-    layername = row [5]
-    title = row [6]
-    cache = row [4]
+    linea = row.split(';')
+    layername = linea [5]
+   
+    title = linea [6]
+    cache = linea [4]
+   
     tupla = cache.partition('file:')
     temp = tupla [2]
     temp2 = temp.partition("'")
     find = temp2 [0]
-    metalink = row [8]
-    styles = row [9]
+    metalink = linea [8]
+    styles = linea [9]
     styles = styles + '.sld'
     tupla = metalink.partition('id=')
     cad = tupla [2]
     num = cad.replace ("'","")
     num = num.replace (")","")
     num = num.replace ("]", "")
-    dire = '/home/diana/geoutils/python/DATA' + '/' + find     
+    dire = '/home/wilson/pythonDiana/DATA' + '/' + find     
+    print num
     if num == '':
 		print 'no tiene metadato' 
     else: 
@@ -55,7 +55,7 @@ for row in datos:
            urllib.urlretrieve(url, filename=metadata)
     #copy the associated SLD
     matches = []
-    for root, dirnames, filenames in os.walk('/home/diana/geoutils/python/DATA/styles'):
+    for root, dirnames, filenames in os.walk('/home/wilson/pythonDiana/DATA/styles'):
         for filename in fnmatch.filter(filenames, styles):  
             matches.append(os.path.join(root, filename))
            
@@ -68,22 +68,20 @@ for row in datos:
 				shutil.copy2(actual, final)
 
 # check if you have the extension prj and if it creates
-    shapefile = dire + '/' + layername + '.shp'
-    # creates an object (ESRI Shapefile)
-    driver = ogr.GetDriverByName('ESRI Shapefile')
-    inDS = driver.Open(shapefile, 0)
-    if inDS is None:
-        print 'Could not open file'
-    referencia = dire + '/' + layername + '.prj'
-    inLayer = inDS.GetLayer()
-    spatialRef = inLayer.GetSpatialRef()
-    if spatialRef == None:
-		geod= 'GEOGCS["GCS_WGS_1984",DATUM["D_WGS_1984",'
-		sphe='SPHEROID/["WGS_1984",6378137.0,298.257223563]],'
-		primen = 'PRIMEM["Greenwich",0.0],UNIT["Degree",0.0174532925199433]]'
-		ref = geod + sphe + primen
-		file = open(referencia, 'w')
-		file.write(ref)
-		file.close()
+#    shapefile = dire + '/' + layername + '.shp'
+#    driver = ogr.GetDriverByName('ESRI Shapefile')
+#    inDS = driver.Open(shapefile, 0)
+#    if inDS is None:
+#        print 'Could not open file'
+ #   referencia = dire + '/' + layername + '.prj'
+#    inLayer = inDS.GetLayer()
+#    spatialRef = inLayer.GetSpatialRef()
+#    if spatialRef == None:
+#		geod= 'GEOGCS["GCS_WGS_1984",DATUM["D_WGS_1984",'
+#		sphe='SPHEROID/["WGS_1984",6378137.0,298.257223563]],'
+#		primen = 'PRIMEM["Greenwich",0.0],UNIT["Degree",0.0174532925199433]]'
+#		ref = geod + sphe + primen
+#		file = open(referencia, 'w')
+#		file.write(ref)
+#		file.close()
 	   	  
-	   
