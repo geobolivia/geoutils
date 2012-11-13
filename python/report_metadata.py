5a03e79530956f23d72d6357790acb47e880c785
Sylvain Lesage <severo@rednegra.net>
2012-11-13 15:52:27 -0400

    subimos la funcion arriba

--- python/report_metadata.py
+++ python/report_metadata.py
@@ -14,6 +14,37 @@ import osgeo.osr
 from osgeo import ogr
 from unidecode import unidecode
 
+# Class for writing in CSV without encoding problems
+# See: http://docs.python.org/2/library/csv.html#csv-examples
+class UnicodeWriter:
+    """
+    A CSV writer which will write rows to CSV file "f",
+    which is encoded in the given encoding.
+    """
+
+    def __init__(self, f, dialect=csv.excel, encoding="utf-8", **kwds):
+        # Redirect output to a queue
+        self.queue = cStringIO.StringIO()
+        self.writer = csv.writer(self.queue, dialect=dialect, **kwds)
+        self.stream = f
+        self.encoder = codecs.getincrementalencoder(encoding)()
+
+    def writerow(self, row):
+        self.writer.writerow([s.encode("utf-8") for s in row])
+        # Fetch UTF-8 output from the queue ...
+        data = self.queue.getvalue()
+        data = data.decode("utf-8")
+        # ... and reencode it into the target encoding
+        data = self.encoder.encode(data)
+        # write to the target stream
+        self.stream.write(data)
+        # empty queue
+        self.queue.truncate(0)
+
+    def writerows(self, rows):
+        for row in rows:
+            self.writerow(row)
+
 def getcswrecords(csw, maxiter=None, maxrecordsinit=None, factormult=None):
     if maxiter is None:
         maxiter=100
@@ -72,36 +103,7 @@ def getrecordfields(r, fieldskeys):
         }
     return [fields[k] for k in fieldskeys]
 
-# Class for writing in CSV without encoding problems
-# See: http://docs.python.org/2/library/csv.html#csv-examples
-class UnicodeWriter:
-    """
-    A CSV writer which will write rows to CSV file "f",
-    which is encoded in the given encoding.
-    """
 
-    def __init__(self, f, dialect=csv.excel, encoding="utf-8", **kwds):
-        # Redirect output to a queue
-        self.queue = cStringIO.StringIO()
-        self.writer = csv.writer(self.queue, dialect=dialect, **kwds)
-        self.stream = f
-        self.encoder = codecs.getincrementalencoder(encoding)()
-
-    def writerow(self, row):
-        self.writer.writerow([s.encode("utf-8") for s in row])
-        # Fetch UTF-8 output from the queue ...
-        data = self.queue.getvalue()
-        data = data.decode("utf-8")
-        # ... and reencode it into the target encoding
-        data = self.encoder.encode(data)
-        # write to the target stream
-        self.stream.write(data)
-        # empty queue
-        self.queue.truncate(0)
-
-    def writerows(self, rows):
-        for row in rows:
-            self.writerow(row)
 
 def prepareforcsv(cswrecords, fieldskeys, fieldsnames):
     matrix=[fieldsnames]
