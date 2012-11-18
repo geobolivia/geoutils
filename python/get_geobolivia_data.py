f657814731fef27d335dec136244629336e979ae
Sylvain Lesage <severo@rednegra.net>
2012-11-18 10:56:02 -0400

    Downloading the SLD style

--- python/get_geobolivia_data.py
+++ python/get_geobolivia_data.py
@@ -4,7 +4,6 @@ args:
 """ 
 from osgeo import ogr
 from osgeo import gdal
-from owslib.csw import CatalogueServiceWeb
 from owslib.wms import WebMapService
 from urlparse import urlparse
 from urlparse import urlunparse
@@ -13,12 +12,17 @@ from urllib import urlretrieve
 import os
 from re import compile
 from re import split
+from geoserver.catalog import Catalog
+import getpass
+
 # Libraries
 # ogr: WFS, SHP
-# http://www.gdal.org/ogr/drv_wfs.html
-# http://www.paolocorti.net/2011/03/23/a-quick-look-at-the-wfs-gdal-driver/
-# owslib: WMS, CSW, SLD
-# http://geopython.github.com/OWSLib/
+#  http://www.gdal.org/ogr/drv_wfs.html
+#  http://www.paolocorti.net/2011/03/23/a-quick-look-at-the-wfs-gdal-driver/
+# owslib: WMS, CSW
+#  http://geopython.github.com/OWSLib/
+# gsconfig: SLD
+#  http://dwins.github.com/gsconfig.py/
 
 def write_xml_metadata(url,filebase):
 	# Code specific to GeoBolivia way to fill the MetadataUrl fields in GeoServer
@@ -31,14 +35,28 @@ def write_xml_metadata(url,filebase):
 	except:
 		pass
 
+def write_sld_style(style,filebase):
+	# TODO: wrap SLD in human-readable style
+	stylefile=filebase+'.sld'
+	with open(stylefile, 'w') as f:
+		f.write(style.sld_body)
+
 # Input arguments
 baseurl='http://www.geo.gob.bo/geoserver/'
 workspacename='otros'
 layername='Ecoregiones_WWF'
-#wmsurl=baseurl+'/'+workspacename+'/wms'
-wmsurl=baseurl+'/'+workspacename+'/'+layername+'/wms'
+wmsurl=baseurl+'/'+workspacename+'/wms'
+#wmsurl=baseurl+workspacename+'/'+layername+'/wms'
 outputpath='/tmp/'
 re_layerid=compile(":")
+
+# Connect to REST GeoServer
+resturl=baseurl+'rest'
+stylesurl=resturl+'/styles/'
+user = raw_input("User: ")
+pw = getpass.getpass('Password: ')
+cat = Catalog(resturl, username=user, password=pw)
+
 
 # Get capabilities for this layer
 wms = WebMapService(wmsurl, version='1.1.1')
@@ -62,4 +80,10 @@ for l in layers:
 	# TODO - manage various Metadata Urls
 	for m in layermd.metadataUrls:
 		write_xml_metadata(m['url'],filebase)
+
+	# Style
+	# TODO - manage various Metadata styles
+	for s in layermd.styles.keys():
+		reststyle=cat.get_style(s)
+		write_sld_style(reststyle,filebase)
 
