a5df348737a12e7b5e6daf1f21396d0b49c290cc
Sylvain Lesage <severo@rednegra.net>
2012-11-20 09:10:14 -0400

    Cleaning owsdl_livedvd and properties

--- python/owsdl_livedvd.py
+++ python/owsdl_livedvd.py
@@ -1,13 +1,10 @@
 #!/usr/bin/python
 import getpass
-import csv
-from geoserver.catalog import Catalog
-
 from downloaders import Downloader
 
 # Input parameters
 import properties
-geoserverurl = properties.baseurl
+geoserverUrl = properties.geoserverUrl
 if properties.user is None:
 	user = raw_input("User: ")
 else:
@@ -16,37 +13,11 @@ if properties.pw is None:
 	pw = getpass.getpass('Password: ')
 else:
 	pw = properties.pw
-csvFilename = properties.csvfilename
-outputpath = properties.outputpath
-firstLayerFilter = properties.firstdata
+csvFilename = properties.csvFilename
+outputPath = properties.outputPath
+firstLayerFilter = properties.firstLayerFilter
 
-## Read CSV file
-#def read_csv(csvfilename):
-#	datalist = []
-#	with open(csvfilename, 'rb') as csvfile:
-#		csvreader = csv.reader(csvfile, delimiter=';', quotechar='"')
-#		for row in csvreader:
-#			datalist.append([row[0], row[1]])
-#	return datalist
-
-#datalist = [('otros', 'mosaico_landsat'), ('otros', 'Spot'), ('inra', 'Predios2012')]
-#datalist = read_csv(csvfilename)
-#if firstdata in datalist:
-#	firstindex = datalist.index(firstdata)
-#	datalist = datalist[firstindex:]
-
-# Loop on the layers
-#for d in datalist:
-#	workspacename = d[0]
-#	layername = d[1]
-#	print 'Download layer ' + workspacename + ':' + layername
-	#layerbaseurl = baseurl + workspacename + '/' + layername + '/'
-#	try:
-d = Downloader(geoserverurl, user, pw)
+d = Downloader(geoserverUrl, user, pw)
 d.addLayersFromWms()
 d.filterLayersFromCsv(csvFilename, firstLayerFilter)
-d.getLayers(outputpath)
-	#gbowsdl.get_workspace(geoserverurl, outputpath, workspacename, layername, user=user, pw=pw)
-#	except Exception as e:
-#		print "  ERROR downloading data for layer " + workspacename + ':' + layername + ": ", e
-#		pass
+d.getLayers(outputPath)
